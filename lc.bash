d637d72de5c0f2316df9060ca6bcefee63fd5202
hmuronaka <capriccio27@gmail.com>
2015-10-27 10:26:06 +0900

    working lc.bash

---
+++ lc.bash
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+num=0
+for f in $(CLICOLOR_FORCE=1 ls -GF); do
+ #/bin/echo -n ${num}: ${f}
+ echo ${num}: ${f}
+ num=$(($num + 1))
+done
+/bin/echo -n "input dir num > "
+read DIR_NUM
+
+num=0
+for f in $(ls -G); do
+ #/bin/echo -n ${num}: ${f}
+ if [ $num -eq $DIR_NUM ]; then
+   cd $f
+   exit 0
+ fi
+ num=$(($num + 1))
+done
